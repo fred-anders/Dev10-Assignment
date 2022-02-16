d0af820ca0b95bba4b8ea0c072108a29afdf99b3
Fred Anderson <fred_anders@outlook.com>
2022-02-15 21:32:08 -0700

    refactored into function and added comments

--- Higher Lower/script.js
+++ Higher Lower/script.js
@@ -1,23 +1,31 @@
-let valid_max = false;
-let between = document.getElementById("between");
-var max = Math.round(Number(window.prompt("what is the maximum number you want to guess?")));
-while (valid_max == false)
-    if (isNaN(max)) {
-        window.alert("This is not a number, try again.");
-        max = Number(window.prompt("what is the maximum number you want to guess?"));
+// This function handles the initial user input. Runs on page load, see below...
+function prompt_input() {
+    let valid_max = false;
+    let between = document.getElementById("between");
+    max = Math.round(Number(window.prompt("what is the maximum number you want to guess?")));
+    // We need a valid max to continue
+    while (false == valid_max) {
+        if (isNaN(max)) {
+            // Not a number!
+            window.alert("This is not a number, try again.");
+            max = Math.round(Number(window.prompt("what is the maximum number you want to guess?")));
+        }
+        else if (max < 1) {
+            // We don't want negative!
+            window.alert("The number must be positive and greater than 1.");
+            max = Math.round(Number(window.prompt("what is the maximum number you want to guess?")));
+        }
+        else {
+            // Valid!
+            valid_max = true;
+            between.innerHTML = `Guess a number between 1 and ${max}`;
+            val = Math.floor(Math.random() * max) + 1;
+            console.log(`${val}`);
+        }
     }
-    else if (max < 1) {
-        window.alert("The number must be positive and greater than 1.");
-        max = Number(window.prompt("what is the maximum number you want to guess?"));
-    }
-    else {
-        valid_max = true;
-        between.innerHTML = `Guess a number between 1 and ${max}`;
-    }
-let val = Math.floor(Math.random() * max) + 1;
-console.log(`${val}`);
-guessArray = [];
+}
 
+// This function handles the guesses. Triggered by button onclick.
 function do_guess() {
     let numGuess = Number(document.getElementById("guess").value);
     console.log(`${numGuess}`);
@@ -25,27 +33,41 @@ function do_guess() {
     feedback.style.color = 'red';
 
     if (isNaN(numGuess)) {
+        // Not a number!
         feedback.innerHTML = "That is not a number!";
         return;
     }
     if (numGuess > max || numGuess < 1) {
+        // Out of range!
         feedback.innerHTML = "That number is not in range, try again.";
         return;
     }
     if (guessArray.indexOf(numGuess) == -1) {
+        // Unique guess push to array
         guessArray.push(numGuess);
+        
         if (numGuess == val) {
+            // Guessed it!
             document.getElementById("feedback").style.color = 'black';
             feedback.innerHTML = `You got it! It took you ${guessArray.length} tries and your guesses were ${guessArray.join(", ")}`;
         }
         else if (numGuess > val) {
+            // Too high
             feedback.innerHTML = "No, try a lower number.";
         }
         else {
+            // Too low
             feedback.innerHTML = "No, try a higher number.";
         }
     } else {
+        // Already guessed don't push to array
         feedback.innerHTML = "You already guessed that!";
     }
 }
 
+// Global variables
+let max = 0;
+let val = 0;
+let guessArray = [];
+// Prompt user for input on page load
+prompt_input();
